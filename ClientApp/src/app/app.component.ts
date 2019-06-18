a928dd6d1f9e436f485036c5f027f021105a1a4f
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-06-18 17:57:01 +0530

    Section 1

---
+++ ClientApp/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Movie } from './models/movie.model';
+import { Repository } from './models/repository';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Web Development with ASP.NET Core & Angular 7';
+
+  constructor(private repo: Repository) {}
+
+  get movie(): Movie {
+    return this.repo.movie;
+  }
+}
