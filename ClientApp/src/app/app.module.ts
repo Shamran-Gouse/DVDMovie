1a3e1f54e4b2f5070c32bf52a6b9dbcab66490be
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-06-19 13:47:49 +0530

    Section 4.1, 4.2 - Placeholders & components AND Completing the Movie List
    Placeholders & components AND Completing the Movie List

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,27 +1,23 @@
+import { StoreModule } from './store/store.module';
 import { RoutingConfig } from './app.routing';
-import { MovieDetailComponent } from './structure/movieDetail.component';
-import { CategoryFilterComponent } from './structure/categoryFilter.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ModelModule } from './models/model.module';
-import { MovieTableComponent } from './structure/movieTable.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MovieTableComponent,
-    CategoryFilterComponent,
-    MovieDetailComponent
+    AppComponent
   ],
   imports: [
     RoutingConfig,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     ModelModule,
-    FormsModule
+    FormsModule,
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
