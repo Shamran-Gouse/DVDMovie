8078353637682745b7434e81e86285ae9c6b4034
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-06-19 20:01:07 +0530

    Section 5.8 - Extending the Session Data

--- Controllers/SessionValuesController.cs
+++ Controllers/SessionValuesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using DVDMovie.Models;
+using DVDMovie.Models.BindingTargets;
 
 namespace DVDMovie.Controllers
 {
@@ -13,12 +14,26 @@ namespace DVDMovie.Controllers
         {
             return Ok(HttpContext.Session.GetString("cart"));
         }
-        
+
         [HttpPost("cart")]
         public void StoreCart([FromBody] MovieSelection[] movies)
         {
             var jsonData = JsonConvert.SerializeObject(movies);
             HttpContext.Session.SetString("cart", jsonData);
         }
+
+        [HttpGet("checkout")]
+        public IActionResult GetCheckout()
+        {
+            return Ok(HttpContext.Session.GetString("checkout"));
+        }
+        
+        [HttpPost("checkout")]
+        public void StoreCheckout([FromBody] CheckoutState data)
+        {
+            HttpContext.Session.SetString("checkout",
+            JsonConvert.SerializeObject(data));
+        }
+
     }
 }
