2729bb282269120148dcdc8605d97d728c336847
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-06-19 19:33:44 +0530

    Section 5.7 - Checkout Process Pt 3

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { StoreModule } from './store/store.module';
-import { RoutingConfig } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ModelModule } from './models/model.module';
+import { RoutingConfig } from './app.routing';
+import { StoreModule } from './store/store.module';
+import { MovieSelectionComponent } from './store/movieSelection.component';
 
 @NgModule({
   declarations: [
