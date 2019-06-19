085a546e5802c765bed45a7164a3aafc7f71be15
Shamran Gouse <shamrangouse.sg@gmail.com>
2019-06-19 14:14:15 +0530

    Section 4.4 - Pagination Controls

--- ClientApp/src/app/store/movieList.component.ts
+++ ClientApp/src/app/store/movieList.component.ts
@@ -3,12 +3,16 @@ import { Repository } from '../models/repository';
 import { Movie } from '../models/movie.model';
 
 @Component({
-    selector: "store-movie-list",
-    templateUrl: 'movieList.component.html'
+  selector: "store-movie-list",
+  templateUrl: 'movieList.component.html'
 })
 export class MovieListComponent {
-    constructor(private repo: Repository) { }
-    get movies(): Movie[] {
-        return this.repo.movies;
+  constructor(private repo: Repository) { }
+
+  get movies(): Movie[] {
+    if (this.repo.movies != null && this.repo.movies.length > 0) {
+      let pageIndex = (this.repo.pagination.currentPage - 1) * this.repo.pagination.moviesPerPage;
+      return this.repo.movies.slice(pageIndex, pageIndex + this.repo.pagination.moviesPerPage);
     }
+  }
 }
